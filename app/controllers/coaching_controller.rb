ad80c0a64159d0bd5764ee48d1fd61ec5c6b0f25
Lucie La Sagna <lucie.lasagna@essec.edu>
2017-02-13 14:17:27 +0100

    exo1

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,10 @@
+class CoachingController < ApplicationController
+  def home
+  end
+
+  def answer
+  end
+
+  def ask
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'coaching#home'
+  #get '/', to: 'coaching#home'
+  get 'answer', to: 'coaching#answer'
+
+  get 'ask', to: 'coaching#ask'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
